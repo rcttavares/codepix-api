646512e79cc92493be41749f7bc133d12b7f8c74
Rafael Tavares <rafael.tawares@gmail.com>
2023-10-20 21:52:07 -0300

    Create bank-accounts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BankAccount } from './bank-accounts/entities/bank-account.entity';
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BankAccountsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      database: 'nest',
+      username: 'postgres',
+      password: 'root',
+      entities: [BankAccount],
+      synchronize: true,
+    }),
+    BankAccountsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/bank-accounts/bank-accounts.module.ts
+++ src/bank-accounts/bank-accounts.module.ts
@@ -1,4 +1,12 @@
+import { BankAccount } from './entities/bank-account.entity';
+import { BankAccountsController } from './bank-accounts.controller';
+import { BankAccountsService } from './bank-accounts.service';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([BankAccount])],
+  controllers: [BankAccountsController],
+  providers: [BankAccountsService],
+})
 export class BankAccountsModule {}
